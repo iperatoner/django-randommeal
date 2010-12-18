0a3a39031ed78738b688e6fa8d6db14568202837
Immanuel Peratoner <immanuel.peratoner@gmail.com>
2010-12-18 18:19:15 +0100

    * Removed MealQuerySet's __add__ method, because it was unnecessary

--- meals/managers.py
+++ meals/managers.py
@@ -17,25 +17,13 @@ class EatenMealManager(django_models.Manager):
         return EatenMealQuerySet(self.model)
 
 class MealQuerySet(QuerySet):
-    def add_or_sub(self, operator, other):
-        assert operator in ['+', '-']
-        if operator == '+':
-            result = set(self) | set(other)
-        elif operator == '-':
-            result = set(self) - set(other)
-            
+    def __sub__(self, other):
+        """Removes the meals of `other` from this queryset."""
+        result = set(self) - set(other)
         meal_ids = []
         for meal in result:
             meal_ids.append(meal.id)
         return self.model.objects.filter(id__in=meal_ids)
-    
-    def __add__(self, other):
-        """Adds the meals of `other` from this queryset."""
-        return self.add_or_sub('+', other)
-    
-    def __sub__(self, other):
-        """Removes the meals of `other` from this queryset."""
-        return self.add_or_sub('-', other)
 
 class MealManager(django_models.Manager):
     def get_query_set(self):
